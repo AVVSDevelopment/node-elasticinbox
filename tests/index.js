e3b21ced996ec4b9bee0cc1bc103bc2647501d20
Vitaly[AVVS] <v@aminev.me>
2013-05-09 22:55:01 +0400

    Account and Label test coverage

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,8 @@ var assert       = require("chai").assert,
     expect       = require('chai').expect,
     ElasticInbox = require("../index.js"),
     config       = require("../config/host.json"),
-    async        = require("async")
+    async        = require("async"),
+    _            = require('lodash')
 
 describe("[Failsafe]", function() {
 
@@ -16,122 +17,197 @@ describe("[Failsafe]", function() {
         })
     })
 
+    var client = new ElasticInbox({
+        "host": config.host,
+        "port": config.port
+    })
+
+    var valid_domain     = "example.com",
+        valid_username   = "test",
+        invalid_domain   = "example,.com",
+        invalid_username = "user^;name",
+        invalid_label    = "invalid^label"
+        valid_label      = "custom_label"
+
     /** Accounts **/
 
-    describe("[Create email: Invalid parameters]", function() {
-        var client = new ElasticInbox({
-            "host": config.host,
-            "port": config.port
-        })
+    describe("Account", function(){
 
-        it("Should return error on invalid parameters", function(done) {
+        describe("#create()", function(){
 
-            async.parallel([
-                async.apply(client.account.create, "test-domain-wrong", "test"),
-                async.apply(client.account.create, "test.com",  "wrong/,email")
-            ], function(error){
-                assert.notEqual(error, null)
-                done()
+            it("shoudn't create account with invalid arguments", function(done){
+                async.parallel([
+                    async.apply(client.account.create, invalid_domain,  invalid_username),
+                    async.apply(client.account.create, valid_domain,    invalid_username),
+                    async.apply(client.account.create, invalid_domain,  valid_username)
+                ], function(error){
+                    assert.notEqual(error, null)
+                    done()
+                })
             })
 
+            it("should create account with valid arguments", function(done){
+                client.account.create(valid_domain, valid_username, function(error, result) {
+
+                    assert.equal(error, null)
+                    assert.propertyVal(result, 'ok', true)
+
+                    done()
+                })
+            })
         })
-    })
 
-    describe("[Create email: Valid parameters]", function() {
-        var client = new ElasticInbox({
-            "host": config.host,
-            "port": config.port
+        describe("#delete()", function(){
+
+            it("should return error on invalid arguments", function(){
+                client.account.delete(invalid_domain, invalid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+
+                client.account.delete(invalid_domain, valid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+
+                client.account.delete(valid_domain, invalid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+            })
+
+            it("should delete account if it exists", function(done){
+                client.account.delete(valid_domain, valid_username, function(error, result){
+                    assert.equal(error, null)
+                    done()
+                })
+            })
         })
 
-        it("Should create user", function(done) {
-            client.account.create("test.com", "test", function(error, result) {
-
-                assert.equal(error, null)
-                assert.propertyVal(result, 'ok', true)
-
-                done()
+        describe("#info()", function(){
+            it("should return account information: not implemented", function(){
+                client.account.info(valid_domain, valid_username, function(err, result){
+                    assert.notEqual(err, null)
+                })
             })
         })
     })
 
-
-    describe("[Delete account: Invalid parameters]", function() {
-        var client = new ElasticInbox({
-            "host": config.host,
-            "port": config.port
-        })
-
-        it("Should return error on invalid parameters", function() {
-
-            client.account.delete("test-domain-wrong", "test,", function(error, result){
-                assert.notEqual(error, null)
-            })
-
-            client.account.delete("test.com", 'wrong,mail', function(error, result){
-                assert.notEqual(error, null)
-            })
-
-        })
-    })
-
-
-    describe("[Delete account: Valid parameters]", function(){
-        var client = new ElasticInbox({
-            "host": config.host,
-            "port": config.port
-        })
-
-        it("Should delete user", function(done){
-           client.account.delete("test.com", "test", function(error, result){
-
-               assert.equal(error, null)
-
-               done()
-           })
-        })
-    })
-
-    describe("[Get account information]", function(){
-        it("Should return account information")
-    })
 
     /** Labels **/
 
-    describe("[Create label: invalid parameters]", function(){
-        var client = new ElasticInbox({
-            "host": config.host,
-            "port": config.port
+    describe("Label", function(){
+
+        describe("#list()", function(){
+
+            it("Should return error on invalid arguments", function(){
+
+                client.label.list(invalid_domain, invalid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+
+                client.label.list(invalid_domain, valid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+
+                client.label.list(valid_domain, invalid_username, function(error, result){
+                    assert.notEqual(error, null)
+                })
+            })
+
+            it("Should return list of labels without metadata", function(done){
+                client.label.list(valid_domain, valid_username, function(error, result){
+                    assert.equal(error, null)
+                    assert.isObject(result, "Label list returned")
+
+                    done()
+                })
+            })
+
+            it("Should return list of labels with metadata", function(done){
+                client.label.list(valid_domain, valid_username, true, function(error, result){
+                    assert.equal(error, null)
+                    assert.isObject(result, "Label list returned")
+
+                    label_ids = Object.keys(result)
+                    label_ids.forEach(function(label_id){
+                        assert.isObject(result[label_id])
+                    })
+
+                    done()
+                })
+            })
+
         })
 
-        it("Should return error on invalid parameters, label cant contain ^ char", function(){
-           client.label.create("test.com", "user", "label^name", function(error, result){
-               assert.notEqual(error, null)
-           })
+        describe("#create()", function(){
+
+            it("Should return error on invalid arguments, label cant contain ^ char", function(){
+                client.label.create(valid_domain, valid_username, invalid_label, function(error, result){
+                    assert.notEqual(error, null)
+                })
+            })
+
+            it("Should create label", function(done){
+                client.label.create(valid_domain, valid_username, valid_label, function(error, result){
+                    assert.equal(error, null)
+                    done()
+                })
+            })
+
         })
-    })
 
-    describe("[Create label: valid parameters]", function(){
-       it("Should create label")
-    })
+        describe("#rename() and #delete()", function(){
 
-    describe("[Delete label: label exists]", function(){
-        it("Should remove existing label")
-    })
+            var label_id,
+                reserved_label = 0
 
-    describe("[Delete label: label doesnt exist]", function(){
-        it("Should throw error saying that the label doesnt exist")
-    })
+            before(function(done){
 
-    describe("[getAll labels for the account: with metadata]", function(){
-        it("Should return labels with metadata")
-    })
+                client.label.list(valid_domain, valid_username, function(error, result){
+                    if (error) return done(error)
 
-    describe("[getAll labels for the account: no metadata]", function(){
-        it("Should return labels without metadata")
-    })
+                    label_id = _.findKey(result, function(label) {
+                        return label == valid_label
+                    })
 
-    describe("[rename label]", function(){
-       it("Should rename label")
+                    if (label_id) return done()
+
+                    client.label.create(valid_domain, valid_username, valid_label, function(error, result){
+                        if (error) return done(error)
+                        label_id = result.id
+                        done()
+                    })
+                })
+            })
+
+            describe("#rename()", function(){
+
+                it("Should rename existing label", function(done){
+                    client.label.rename(valid_domain, valid_username, label_id, valid_label+":rename", function(err, result){
+                        assert.equal(err, null)
+                        done()
+                    })
+                })
+
+            })
+
+            describe("#delete()", function(){
+
+                it("Should remove existing label", function(done){
+                    client.label.delete(valid_domain, valid_username, label_id, function(err, result){
+                        assert.equal(err, null)
+                        done()
+                    })
+                })
+
+                it("Should not remove reserved label", function(done){
+                    client.label.delete(valid_domain, valid_username, reserved_label, function(err, result){
+                        assert.notEqual(err, null)
+                        done()
+                    })
+                })
+
+            })
+        })
+
     })
 
 
